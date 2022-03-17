c5be1f854fb0dfd66cee07283d083d946989d103
IT21180620 <99192440+IT21180620@users.noreply.github.com>
2022-03-17 11:43:14 +0000

    completed

--- Box.cpp
+++ Box.cpp
@@ -2,6 +2,25 @@
 
 // Implement setters and getters
 
+void Box::setMeasurements(int l, int w, int h){
+  width = l;
+  height = w;
+  length = h;
+}
+
+int Box::getWidth(){
+  return width;
+}
+
+int Box::getHeight(){
+  return height;
+}
+
+int Box::getLength(){
+  return length;
+}
+
 // Implemenet the calcVolume() unction
 int Box::calcVolume() {
+  return width * height * length;
 }
